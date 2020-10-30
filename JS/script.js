2b1a3b51afdfc5af5fa6063b380a064c8f15e8df
Adebimpe <realadebimpe@gmail.com>
2020-10-30 22:51:59 +0100

    Refactored all files

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,23 @@
-var displayScreen = document.getElementById("displayScreen");
-var displayWord = document.getElementById("displayWord");
-var generatorButton = document.getElementById("generatorButton");
+const displayWord = document.getElementById("displayWord");
+const generatorButton = document.getElementById("generatorButton");
+
+const words = ['Bimpe', 'like', 'Bicycle', 'Yam', 'Generous', 'Happy', 'Contentment', 'lamentable', 'share', 'undesirable', 'face', 'consen', 'conquer',
+'Apparel', 'Tender', 'Economic', 'Trick', 'Charming', 'Violate', 'Slope', 'Lead', 'Sound', 'Enchanted',
+'Shelf', 'Hilarious', 'Ordinary', 'Sweet', 'Gaze', 'Odd', 'Behold', 'Gold', 'Boundless'];
+
+function generateRandomNum(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+
+}
+
+function buttonHandler(event) {
+    const index = generateRandomNum(words.length);
+    
+
+    const word = words[index];
+  
+
+    displayWord.innerText = word;
+}
+
+generatorButton.addEventListener('click', buttonHandler)
